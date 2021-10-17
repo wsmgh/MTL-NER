08c0d1c78f04ea23d6b6db8aae655a7013968e24
wsmgh <38832437+wsmgh@users.noreply.github.com>
2021-10-17 18:29:03 +0800

    完成对prune模块的调试

--- prune.py
+++ prune.py
@@ -15,7 +15,8 @@ def apply_mask_to_model(model, mask):
     '''
     for n, p in model.named_parameters():
         if n in mask:
-            p.data.masked_fill_(mask[n] == 0, 0.)
+            m=(mask[n] == 0).to(p.data.device)
+            p.data.masked_fill_(m, 0.)
 
 
 def apply_mask_to_grad(model, mask):
@@ -26,13 +27,28 @@ def apply_mask_to_grad(model, mask):
     '''
     for n, p in model.named_parameters():
         if n in mask:
-            p.grad.masked_fill_(mask[n] == 0, 0.)
+            m = (mask[n] == 0).to(p.grad.device)
+            p.grad.masked_fill_(m, 0.)
+
+
+def sparsity(mask):
+    '''
+    calculate the sparsity of a sub-net through mask
+    :param mask:
+    :return:
+    '''
+    x,y=0,0
+    for name in mask:
+        x+=torch.sum(mask[name])
+        y+=torch.sum(torch.ones(mask[name].shape))
+
+    return x/y
 
 
 
 class Prune:
 
-    def __init__(self,model,tasks,pruning_rate,sparsity,names,warmup_steps,word2id,char2id,device):
+    def __init__(self,model,tasks,pruning_rate,sparsity,names,warmup_steps,word2id,char2id,path,device):
 
         '''
         :param model: the model to be pruned
@@ -50,6 +66,7 @@ class Prune:
         self.mask_dict={}
         self.warmup_steps=warmup_steps
         self.init_params=None
+        self.path=path
 
         self.word2id=word2id
         self.char2id=char2id
@@ -57,7 +74,10 @@ class Prune:
 
 
     def pruning(self):
-
+        '''
+        the main function for pruning
+        :return:
+        '''
         self.init_model()
         self.save_model_params()
         for task in self.tasks:
@@ -66,7 +86,7 @@ class Prune:
             mask={}
             for name,params in self.model.named_parameters():
                 if self.need_prune(name):
-                   mask[name]=torch.ones(params.shape)
+                   mask[name]=torch.ones(params.shape).to(self.device)
 
             while(True):
 
@@ -75,24 +95,26 @@ class Prune:
                 # inplace
                 self.prune(mask)
 
-                masks.append(mask)
+                masks.append(copy.deepcopy(mask))
 
-                if self.sparsity(mask)<self.min_sparsity:
+                if sparsity(mask)<self.min_sparsity:
                     break
                 else:
                     self.resume_params()
             
-            self.mask_dict[task.t_id]=masks
+            self.mask_dict[task.t_name]=masks
+
+        self.save()
 
 
 
-    def save(self,path=''):
+    def save(self):
         '''
-        save self.mask_dict to path
+        save self.mask_dict to self.path
         :param path:
         :return:
         '''
-        torch.save(self.mask_dict,path)
+        torch.save(self.mask_dict,self.path)
 
 
     def init_model(self):
@@ -120,6 +142,7 @@ class Prune:
     def warmup(self, task, mask):
         '''
         warmup the sub-net
+        just modify this function when adapt to different model
         :param t_id:
         :param mask:
         :return:
@@ -130,7 +153,7 @@ class Prune:
         for step in trange(self.warmup_steps):
 
             batch=next(dl_it)
-            batch = tokenize(batch, task.label2id, self.word2id, self.char2id, self.device)
+            batch = tokenize(batch, task.label2id, self.word2id, self.char2id,self.device)
 
             loss = self.model.forward_loss(batch['word_ids'], batch['char_ids_f'], batch['word_pos_f']
                                               , batch['char_ids_b'], batch['word_pos_b'], batch['label_ids']
@@ -148,31 +171,20 @@ class Prune:
         :return:
         '''
 
-        params=torch.tensor([])
+        params=torch.tensor([]).to(self.device)
         for n,p in self.model.named_parameters():
-            params=torch.cat([params,p.data[mask[n]==1]],dim=0)
+            if n in mask:
+                params=torch.cat([params,p.data[mask[n]==1]],dim=0)
+        params=torch.abs(params)
         params=torch.sort(params).values
 
         threshold=params[int(self.pr*params.shape[0])]
 
         for n, p in self.model.named_parameters():
-            mask[n]=torch.where(p.data<threshold,torch.zeros(p.data.shape),mask[n])
+            if n in mask:
+                zero=torch.zeros(p.data.shape).to(p.data.device)
+                mask[n]=torch.where(torch.abs(p.data)<threshold,zero,mask[n])
 
-
-
-
-    def sparsity(self, mask):
-        '''
-        calculate the sparsity of a sub-net through mask
-        :param mask:
-        :return:
-        '''
-        x,y=0,0
-        for name in mask:
-            x+=torch.sum(mask[name])
-            y+=torch.sum(torch.ones(mask[name].shape))
-
-        return x/y
 
 
     def resume_params(self):
@@ -205,10 +217,19 @@ if __name__=='__main__':
         task=Task(dataset_name[i],i,datas[i]['train'],datas[i]['devel'],datas[i]['test'],1)
         tasks.append(task)
 
-    model=torch.nn.Linear(10,10)
-    device=torch.device('cpu')
-    tem_p=Prune(model,tasks,0.1,0.5,['weight'],1,word2id,char2id,device)
+    ds_info = []
 
+    for data in datas:
+        ds_info.append(get_dataset_info(data['train'] + data['devel'] + data['test']))
+
+    device=torch.device('cuda')
+
+    model = MTL_BC(len(vocab), len(char), w_emb_size=200, c_emb_size=30, w_hiden_size=300, c_hiden_size=600,
+                   dropout_rate=0.5, ds=ds_info, device=device).to(device)
+
+    tem_p=Prune(model,tasks,0.1,0.5,['linear'],1,word2id,char2id,'masks.pt',device)
+
+    tem_p.pruning()
 
 
     print('done')
